675cf51daf7db9116870afb3d2999b72859f7269
sena <sena@sena-MS-7817>
2017-05-02 09:01:21 -0500

    Practice four

---
+++ src/javaapplication10/Persona.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author sena
+ */
+public class Persona {
+    private Carro carr;
+    private String id;
+    private String tallaCamisa;
+    private int edad;
+    
+    public void setCarr(Carro carrP){
+    this.carr=carrP;
+    }
+    public Carro getCarr(){
+    return carr;
+    }
+    
+    public void setId(String idP)
+    {
+    this.id=idP;
+    }
+     public String getId()
+     {
+     return this.id;
+     }
+     public void caminar(){
+            System.out.println("Estoy caminando");
+     }
+}
